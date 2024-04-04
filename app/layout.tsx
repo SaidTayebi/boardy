0616256c6941cf0992d3bbe466541538d0107fb0
Saïd Tayebi <saidtayebi@gmail.com>
2024-04-04 20:09:58 +0200

    fix(build)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ConvextClientProvider } from "@/providers/convex-client-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { ModalProvider } from "@/providers/modal-provider";
+import { Suspense } from "react";
+import { Loading } from "@/components/auth/loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,11 +22,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvextClientProvider>
-          <Toaster />
-          <ModalProvider />
-          {children}
-        </ConvextClientProvider>
+        <Suspense fallback={<Loading />}>
+          <ConvextClientProvider>
+            <Toaster />
+            <ModalProvider />
+            {children}
+          </ConvextClientProvider>
+        </Suspense>
       </body>
     </html>
   );
